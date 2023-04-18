fb414b9f3bc43fe8482a271d15e6100fb39d8e00
massone99 <lorenzomassone@gmail.com>
2023-04-18 10:00:52 +0200

    Finished Exercises 1-2

--- src/main/scala/u06lab/code/Combiner.scala
+++ src/main/scala/u06lab/code/Combiner.scala
@@ -2,16 +2,30 @@ package u06lab.code
 
 /** 1) Implement trait Functions with an object FunctionsImpl such that the code in TryFunctions works correctly. */
 
+object GivenCombiners:
+  given Combiner[Double] = new Combiner[Double]:
+    def unit: Double = 0.0
+    def combine(a: Double, b: Double): Double = a + b
+  given Combiner[String] = new Combiner[String]:
+    def unit: String = ""
+    def combine(a: String, b: String): String = a + b
+  given Combiner[Int] = new Combiner[Int]:
+    def unit: Int = Int.MinValue
+    def combine(a: Int, b: Int): Int = if a > b then a else b
+
+
 trait Functions:
   def sum(a: List[Double]): Double
   def concat(a: Seq[String]): String
   def max(a: List[Int]): Int // gives Int.MinValue if a is empty
-
+  def combiner[A: Combiner](a: Seq[A]): A
 object FunctionsImpl extends Functions:
-  override def sum(a: List[Double]): Double = ???
-  override def concat(a: Seq[String]): String = ???
-  override def max(a: List[Int]): Int = ???
-
+  import GivenCombiners.given
+  override def sum(a: List[Double]): Double = combiner(a)
+  override def concat(a: Seq[String]): String = combiner(a)
+  override def max(a: List[Int]): Int = combiner(a)
+  override def combiner[A: Combiner](a: Seq[A]): A =
+    a.foldLeft(summon[Combiner[A]].unit)((acc, x) => summon[Combiner[A]].combine(acc, x))
 /*
  * 2) To apply DRY principle at the best,
  * note the three methods in Functions do something similar.
@@ -30,6 +44,8 @@ trait Combiner[A]:
   def combine(a: A, b: A): A
 
 @main def checkFunctions(): Unit =
+  import GivenCombiners.given
+
   val f: Functions = FunctionsImpl
   println(f.sum(List(10.0, 20.0, 30.1))) // 60.1
   println(f.sum(List())) // 0.0
