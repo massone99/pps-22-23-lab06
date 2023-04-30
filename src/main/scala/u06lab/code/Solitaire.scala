539e067868bc5805c8a8a892b5437820522b7b27
massone99 <lorenzomassone@gmail.com>
2023-04-30 16:53:40 +0200

    Finished Solitaire

--- src/main/scala/u06lab/code/Solitaire.scala
+++ src/main/scala/u06lab/code/Solitaire.scala
@@ -2,54 +2,99 @@ package u06lab.code
 
 import org.scalactic.source.Position
 
+import scala.collection.immutable.TreeSeqMap
+import scala.math
+
 /**
  * An object modelling a Solitaire game.
  */
 object Solitaire extends App:
 
-  type Position = (Int, (Int, Int))
-  type Board = (Int, Int)
-  type Solution = List[Position]
+  import SolitaireExtensionMethods.*
 
-  def placeMarks(board: Board) =
-    val width = board._1
-    val height = board._2
+  // e.g. (0, (0,0)), where _._1 is the value of the cell and _._2 is the position of the cell
+  type Cell = (Int, Position)
+  type Position = (Int, Int)
+  type Size = (Int, Int)
+  // It can also represent a partial solution
+  type Board = Seq[Cell]
 
-    val startPos: Position = (1, (width / 2, height / 2))
-    getPossiblePositions(width, height)
+  private def solve(board: Board)(using size: Size): Seq[Board] =
+    import SolitaireExtensionMethods.*
+    if board.isSolved then Seq(board)
+    else
+      val possibleMoves: Seq[Position] = board.getDiagonalJumps ++ board.getCardinalJumps
+      possibleMoves.flatMap(move => solve(board.setCurrentPosition(move)))
 
-
-  def getPossiblePositions(width: Int, height: Int): Seq[(Int, Int)] =
-    for
-      i <- 0 until width
-      j <- 0 until height
-    yield (i, j)
-
-  def render(solution: Seq[(Int, Int)], width: Int, height: Int): String =
-    val reversed = solution.reverse
+  private def drawBoard(board: Board)(using size: Size): String =
     val rows =
-      for y <- 0 until height
-          row = for x <- 0 until width
-                    number = reversed.indexOf((x, y)) + 1
+      for y <- 0 until size._2
+          row = for x <- 0 until size._1
+                    number = board.find(_._2 == (x, y)).get._1
           yield if number > 0 then "%-2d ".format(number) else "X  "
       yield row.mkString
     rows.mkString("\n")
 
-  extension (p: Position)
-    private def isJumpPossible(dest: Position): Boolean =
-      // Get coordinates of the current position
-      val (x, y) = p._2
-      def isDiagonalJumpPossible: Boolean =
-        (for
-          x1 <- (x-2) to (x+2) by 4
-          y1 <- (y-2) to (y+2) by 4
+
+  object SolitaireExtensionMethods:
+    extension (board: Board)(using size: Size)
+      private def isCellInBoard(pos: Position): Boolean = (pos._1 >= 0 && pos._1 < size._1) && (pos._2 >= 0 && pos._2 < size._2)
+      def setStartingPosition(pos: Position): Board =
+        if isCellInBoard(pos) then board.map(p => if p._2 == pos then (1, pos) else p) else throw IllegalArgumentException("Position is not in the board")
+      def setCurrentPosition(pos: Position): Board =
+        val currPos: Cell = board.findCurrentPosition
+        board.map(p => if p._2 == pos then (currPos._1 + 1, pos) else p)
+      def findCurrentPosition: Cell = board.maxBy(_._1)
+      def getValueOfCellFromPosition(pos: Position): Int = board.find(_._2 == pos).get._1
+      def getDiagonalJumps: Seq[Position] =
+        val dist = 1
+        val currPos: Cell = board.findCurrentPosition
+        val (x, y) = currPos._2
+        for
+          i <- -dist to dist by 2
+          j <- -dist to dist by 2
+          jumpableCell = (x + i, y + j)
+          if isCellInBoard(jumpableCell)
+          if getValueOfCellFromPosition(jumpableCell) == 0
         yield
-          println(s"($x1, $y1)")
-          (0, (x1 + 2, y1 + 2))).contains(dest)
-      isDiagonalJumpPossible
+          jumpableCell
 
-  val testPos: Position = (0,(1,1))
-  val testDest: Position = (0,(4,4))
-  val value = testPos.isJumpPossible(testDest)
+      def getCardinalJumps: Seq[Position] =
+        val dist = 2
+        val currPos: Cell = board.findCurrentPosition
+        val (x, y) = currPos._2
+        (for
+          i <- -dist to dist by 2
+          jumpableCell = (x + i, y)
+          if isCellInBoard(jumpableCell)
+          if getValueOfCellFromPosition(jumpableCell) == 0
+        yield jumpableCell)
+          .appendedAll(
+            for
+              j <- -dist to dist by 2
+              jumpableCell = (x, y + j)
+              if isCellInBoard(jumpableCell)
+              if getValueOfCellFromPosition(jumpableCell) == 0
+            yield jumpableCell)
 
-  println(render(solution = Seq((0, 0), (2, 1)), width = 3, height = 3))
+      def isSolved: Boolean = getDiagonalJumps.isEmpty && getCardinalJumps.isEmpty
+
+
+  //println(render(solution = Seq((0, 0), (2, 1)), width = 3, height = 3))
+
+  given size: Size = (5, 5)
+
+  val board: Board = for i <- 0 until size._1; j <- 0 until size._2 yield (0, (i, j))
+
+  private val solutions: Seq[Board] = solve(board.setStartingPosition((size._1 / 2, size._2 / 2)))
+
+  // Filter all the solutions that have contain at least a cell with value 0
+  private val solutionsWithoutZero: Seq[Board] = solutions.filter(_.forall(_._1 != 0))
+
+  println("Number of possible solutions: " + solutions.size)
+  println("Number of possible solutions without 0: " + solutionsWithoutZero.size)
+  for
+    elem <- solutionsWithoutZero.head
+  do
+    println(elem)
+  println(drawBoard(solutionsWithoutZero.head))
--- src/test/scala/u06lab/code/SolitaireTest.scala
+++ src/test/scala/u06lab/code/SolitaireTest.scala
@@ -1,10 +1,96 @@
 package u06lab.code
 
-import org.scalatest.funsuite.AnyFunSuite
+import org.junit.Assert.*
+import org.junit.Test
+import u06lab.code.Solitaire.*
+import u06lab.code.Solitaire.SolitaireExtensionMethods.*
 
-class SolitaireTest extends AnyFunSuite:
-  // Test the method getPossiblePositions
-  test("getPossiblePositions") {
-    val positions = Solitaire.getPossiblePositions(3, 3)
-    println(positions)
-  }
+
+class SolitaireTest:
+  @Test
+  def testSetStartingPositionAndCurrentPosition(): Unit =
+    given size: Size = (2, 2)
+
+    var board: Board = for i <- 0 until size._1; j <- 0 until size._2 yield (0, (i, j))
+    board = board.setStartingPosition((1, 1))
+    assertEquals((1, 1), board.findCurrentPosition._2)
+
+  @Test
+  def testGetValueOfCellFromPosition(): Unit =
+    given size: Size = (2, 2)
+
+    var board: Board = for i <- 0 until size._1; j <- 0 until size._2 yield (0, (i, j))
+    board = board.setStartingPosition((1, 1))
+    assertEquals(1, board.getValueOfCellFromPosition((1, 1)))
+
+  @Test
+  def testGetDiagonalJumps(): Unit =
+    given size: Size = (2, 2)
+
+    var board: Board = for i <- 0 until size._1; j <- 0 until size._2 yield (0, (i, j))
+    board = board.setStartingPosition((1, 1))
+    val jumps = board.getDiagonalJumps
+    println(jumps)
+    assertEquals(List((0, 0)), jumps)
+
+  @Test
+  def testGetDiagonalJumpsWithSmallGrid(): Unit =
+    given size: Size = (2, 2)
+
+    var board: Board = for i <- 0 until size._1; j <- 0 until size._2 yield (0, (i, j))
+    board = board.setStartingPosition((1, 1))
+    val jumps = board.getDiagonalJumps
+    assertEquals(1, jumps.length)
+
+  @Test
+  def testGetCardinalJumps(): Unit =
+    given size: Size = (5, 5)
+
+    var board: Board = for i <- 0 until size._1; j <- 0 until size._2 yield (0, (i, j))
+    board = board.setStartingPosition((2, 2))
+    val jumps = board.getCardinalJumps
+    println(jumps)
+    assertEquals(List((0, 2), (4, 2), (2, 0), (2, 4)), jumps)
+
+  @Test
+  def testGetCardinalJumpsWithSmallGrid(): Unit =
+    given size: Size = (2, 2)
+
+    var board: Board = for i <- 0 until size._1; j <- 0 until size._2 yield (0, (i, j))
+    board = board.setStartingPosition((2, 2))
+    val jumps = board.getCardinalJumps
+    println(jumps)
+    assertEquals(List.empty, jumps)
+
+  @Test
+  def testSetCurrentPosition(): Unit =
+    given size: Size = (3, 3)
+
+    var board: Board = for i <- 0 until size._1; j <- 0 until size._2 yield (0, (i, j))
+    board = board.setStartingPosition((size._1 / 2, size._2 / 2))
+    // Get available jumps
+    val jumps: Seq[Position] = board.getDiagonalJumps
+    board = board.setCurrentPosition(jumps.head)
+    println(board)
+    assertEquals(jumps.head, board.findCurrentPosition._2)
+
+
+  @Test
+  def testSetCurrentPositionIntensive(): Unit =
+    given size: Size = (3, 3)
+
+    var board: Board = for i <- 0 until size._1; j <- 0 until size._2 yield (0, (i, j))
+    board = board.setStartingPosition((size._1 / 2, size._2 / 2))
+    println(board)
+
+    var jumps = board.getDiagonalJumps ++ board.getCardinalJumps
+    while
+      jumps.nonEmpty
+    do
+      val head = jumps.head
+      board = board.setCurrentPosition(head)
+      jumps = board.getDiagonalJumps ++ board.getCardinalJumps
+      println("Jumps size: " + jumps.size)
+      println(board)
+
+    assertTrue(board.isSolved)
